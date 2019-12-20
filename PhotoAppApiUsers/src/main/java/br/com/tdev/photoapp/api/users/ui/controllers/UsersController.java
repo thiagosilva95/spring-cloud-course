34b4f1d3f39a39f72dad6908621795ca8d3c2a54
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-12-20 17:42:08 -0300

    Evaluating Users microservice

--- PhotoAppApiUsers/src/main/java/br/com/tdev/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/br/com/tdev/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,14 +1,23 @@
 package br.com.tdev.photoapp.api.users.ui.controllers;
 
+import javax.validation.Valid;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.tdev.photoapp.api.users.service.UsersService;
+import br.com.tdev.photoapp.api.users.shared.UserDto;
 import br.com.tdev.photoapp.api.users.ui.model.CreateUserRequestModel;
+import br.com.tdev.photoapp.api.users.ui.model.CreateUserResponseModel;
 
 @RestController
 @RequestMapping("/users")
@@ -17,14 +26,26 @@ public class UsersController {
 	@Autowired
 	private Environment env;
 	
+	@Autowired
+	private UsersService usersService;
+	
 	@GetMapping("/status/check")
 	public String status() {
 		return "Working on port: " + env.getProperty("local.server.port");
 	}
 	
 	@PostMapping
-	public String createUser(@RequestBody CreateUserRequestModel userDetails) {
-		return "Create user method is called";
+	public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
+		
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		
+		UserDto userDto = modelMapper.map(userDetails, UserDto.class);
+		UserDto userCreated = usersService.createUser(userDto);
+		
+		CreateUserResponseModel responseModel = modelMapper.map(userCreated, CreateUserResponseModel.class);
+		
+		return ResponseEntity.created(null).body(responseModel);
 	}
 	
 }

11019477a556aa41177d3bc5439eaf1c13e023e1
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-12-20 15:59:38 -0300

    Creating Users microservice

---
+++ PhotoAppApiUsers/src/test/java/br/com/tdev/photoapp/api/users/PhotoAppApiUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tdev.photoapp.api.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppApiUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

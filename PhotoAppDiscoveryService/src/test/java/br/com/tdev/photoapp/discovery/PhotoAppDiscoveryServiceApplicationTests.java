f8830a0ad27dfde0f75be72de2cb4daccb692c02
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-12-20 15:43:17 -0300

    Creating Eureka Server

---
+++ PhotoAppDiscoveryService/src/test/java/br/com/tdev/photoapp/discovery/PhotoAppDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tdev.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
